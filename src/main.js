27074e868918a5d2191fbf61c200617774059a1c
jinzhiqiang99 <539987443@qq.com>
2019-07-27 15:59:14 +0800

    手动配置路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入路由实例
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
+  // 将路由实例挂载到vue实例上
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,13 @@
+// 引入vue
+import Vue from 'vue'
+// 引入router
+import VueRouter from 'vue-router'
+
+// 路由模块化后需要使用vue安装
+Vue.use(VueRouter)
+
+// 创建路由对象
+const router = new VueRouter({})
+
+// 挂载
+export default router
